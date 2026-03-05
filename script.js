ece626f970890c81d2ea20ecf0a5a3a6427e938a
Artjom L <artjom.lugovets@techno.ee>
2026-03-05 18:48:53 +0200

    Teine commit

--- script.js
+++ script.js
@@ -1,37 +1,55 @@
 const button = document.querySelector("#click");
 const input = document.querySelector("#messageInput");
 const messages = document.querySelector("#messageList");
+const userBtn = document.querySelector("#user");
 
-const messageArray = [];
+let username = "";
+
+function setUsername() {
+  username = prompt('Enter your name')
+}
 
 async function getMessages() {
-  const response = await fetch ('https://jsonplaceholder.typicode.com/posts');
-  const messages = await response.json();
-
-  messages.innerHTML = '';
-  messages.forEach(msg => {
-    const messageDiv = document.createElement('div');
-
-  })
-};
-
-async function sendMessage() {
-  const messageText = input.value;
-
-  const msg = { user: , message: messageText };
-
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(messageData)
-  });
-
+  const response = await fetch ('https://tinkr.tech/sdb/myprojekt2');
   const data = await response.json();
 
-  messages.innerHTML += `<p>You: ${messageData}</p>`;
-  messages.innerHTML += `<p>Server received the message!</p>`;
+  messages.innerHTML = "";
 
-  input.value = "";
+  data.forEach(msg => {
+    const p = document.createElement("p");
+    p.innerHTML = `${msg.username}: ${msg.message}`;
+    messages.appendChild(p);
+  });
 };
 
+
+async function sendMessage() {
+  if (!username) {
+    alert("Set username first")
+    return;
+  }
+  if (!input.value) {
+    alert("Write something!")
+    return;
+  }
+
+  const messageText = input.value;
+  let newMessage = {
+    username: username,
+    message: messageText
+  };
+
+  const response = await fetch('https://tinkr.tech/sdb/myprojekt2', {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(newMessage)
+  });
+
+  input.value = "";
+
+  await getMessages();
+};
+
+userBtn.addEventListener("click", setUsername);
 button.addEventListener("click", sendMessage);
+getMessages();
